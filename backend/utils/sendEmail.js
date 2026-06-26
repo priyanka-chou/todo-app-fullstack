60704ede72ccea4720d6c67aa326c8c2190d0f3b
priyanka-chou <164023075+priyanka-chou@users.noreply.github.com>
2026-06-26 23:40:39 +0530

    use brevo api

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,32 +1,33 @@
-const { Brevo } = require("@getbrevo/brevo");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const sendEmail = async (email, otp) => {
   try {
-    await Brevo.transactionalEmails.sendTransacEmail(
-      {
-        sender: {
-          name: "Todo App",
-          email: process.env.BREVO_SENDER,
-        },
-        to: [{ email }],
-        subject: "Todo App OTP Verification",
-        htmlContent: `
-          <h2>Your OTP is: ${otp}</h2>
-          <p>Valid for 5 minutes.</p>
-        `,
-      },
-      {
-        headers: {
-          "api-key": process.env.BREVO_API_KEY,
-        },
-      }
-    );
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
+    sendSmtpEmail.sender = {
+      email: process.env.BREVO_SENDER,
+      name: "Todo App",
+    };
+
+    sendSmtpEmail.to = [{ email }];
+    sendSmtpEmail.subject = "Todo App OTP Verification";
+    sendSmtpEmail.htmlContent = `
+      <h2>Your OTP is: ${otp}</h2>
+      <p>Valid for 5 minutes.</p>
+    `;
+
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
     console.log("Mail sent successfully");
-    return true;
-  } catch (error) {
-    console.log("Mail Error:", error);
-    throw error;
+  } catch (err) {
+    console.log("Mail Error:", err);
+    throw err;
   }
 };
 
