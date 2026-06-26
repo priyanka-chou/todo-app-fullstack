92114e54d6f170291aa98ca0a2d723694f3d24ea
priyanka-chou <164023075+priyanka-chou@users.noreply.github.com>
2026-06-26 23:36:03 +0530

    use brevo api

--- frontend/React/src/api/axios.js
+++ frontend/React/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "https://todo-backend-xxxx.onrender.com"
-    baseURL: "todo-app-fullstack-production-ceda.up.railway.app"
+    baseURL: "https://todo-app-fullstack-production-ceda.up.railway.app"
     //  baseURL: "http://localhost:5000"
 });
 
